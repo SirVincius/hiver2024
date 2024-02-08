e17a5dfd1db709f56a81ea508b33c8d6b656faba
Vincent Brunelle <vincent.brunelle.gm@gmail.com>
2024-02-08 14:24:27 -0500

    .

--- 5190/app.py
+++ 5190/app.py
@@ -1,9 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return render_tamplate("app/templates/index.html")
+def home():
+        return render_template("form.html")
+    
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if request.method == ("GET"):
+        return render_template("form.html")
+    else:
+        name = request.form['name']
+        firstname = request.form['firstname']
+        email = request.form['email']
+        emailconfirmation = request.form['emailconfirmation']
+        password = request.form['password']
+        
+        return render_template("form.html", name=name, firstname=firstname, email=email, emailconfirmation=emailconfirmation, password=password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
